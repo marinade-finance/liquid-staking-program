1238cafe0b402ea329dd91006eda7b9ec7561c6b
Lucio Tato <luciotato@gmail.com>
2023-07-04 08:24:12 -0300

    deposit & deposit-stake-account

--- programs/marinade-finance/src/events/user.rs
+++ programs/marinade-finance/src/events/user.rs
@@ -4,15 +4,15 @@ use anchor_lang::prelude::*;
 pub struct DepositStakeAccountEvent {
     pub state: Pubkey,
     pub stake: Pubkey,
+    pub delegated: u64, // lamports in the stake account delegation
+    pub withdrawer: Pubkey, // withdraw auth for the stake account
     pub stake_index: u32,
     pub validator: Pubkey,
     pub validator_index: u32,
-    pub delegated: u64,
-    pub withdrawer: Pubkey,
+    pub validator_active_balance: u64,
+    pub total_active_balance: u64,
+    pub user_msol_balance: u64,
     pub msol_minted: u64,
-    pub new_user_msol_balance: u64,
-    pub new_validator_active_balance: u64,
-    pub new_total_active_balance: u64,
     // MSOL price used
     pub total_virtual_staked_lamports: u64,
     pub msol_supply: u64,
@@ -22,15 +22,15 @@ pub struct DepositStakeAccountEvent {
 pub struct DepositEvent {
     pub state: Pubkey,
     pub sol_owner: Pubkey,
+    pub user_sol_balance: u64,
+    pub user_msol_balance: u64,
+    pub sol_leg_balance: u64,
+    pub msol_leg_balance: u64,
+    pub reserve_balance: u64,
     pub sol_swapped: u64,
     pub msol_swapped: u64,
     pub sol_deposited: u64,
     pub msol_minted: u64,
-    pub new_user_sol_balance: u64,
-    pub new_user_msol_balance: u64,
-    pub new_sol_leg_balance: u64,
-    pub new_msol_leg_balance: u64,
-    pub new_reserve_balance: u64,
     // MSOL price used
     pub total_virtual_staked_lamports: u64,
     pub msol_supply: u64,
